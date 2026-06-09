1f98b53518cf887ebb9f181f88008001a0ea7bbd
vitorcoutinho01 <vitorcoutinho001@gmail.com>
2026-06-09 00:21:40 +0000

    Fiz o desafio nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,8 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;
 
     char estado2;
     char codigo2[4];
@@ -22,6 +24,8 @@ int main() {
     float area2;
     float pib2;
     int pontosTuristicos2;
+     float densidade2;
+    float pibPerCapita2;
     
   // Área para entrada de dados
 
@@ -86,6 +90,8 @@ int main() {
     printf("Area: %.2f km2\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     printf("Carta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -95,6 +101,8 @@ int main() {
     printf("Area: %.2f km2\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
 
 return 0;
